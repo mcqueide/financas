7fb7a9f5253660351ff1efbcfac25de699c8948b
john.clemente <john.clemente@johnclemente.foton.local>
2015-04-29 11:54:55 -0300

    Termino do vídeo segunda aula

--- src/br/com/caelum/financas/teste/TesteJPA.java
+++ src/br/com/caelum/financas/teste/TesteJPA.java
@@ -4,16 +4,29 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.caelum.financas.modelo.Conta;
+
 public class TesteJPA {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("financas");
-		
+		Conta conta = new Conta();
+		conta.setTitular("Joao Ferreira");
+		conta.setBanco("HSBC");
+		conta.setNumero("123345");
+		conta.setAgencia("321");
+
+		EntityManagerFactory entityManagerFactory = Persistence
+				.createEntityManagerFactory("financas");
+
 		EntityManager manager = entityManagerFactory.createEntityManager();
-		
+
+		manager.getTransaction().begin();
+		manager.persist(conta);
+		manager.getTransaction().commit();
+
 		manager.close();
-		
+
 	}
 
 }
